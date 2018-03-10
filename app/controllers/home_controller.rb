4fb1492890e3930c4f49ff817b5b754a51ed3d20
Romain Endelin <rendelin@zendesk.com>
2018-03-10 18:04:35 +0100

    proper SPA structure

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    render react_component: 'Hello', props: { name: 'a component rendered from a controller' }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
 
-  root to: "home#index"
+  root to: "pages#home"
+
+  get "*path", to: "pages#home"
 end
