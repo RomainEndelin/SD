c5fb9ce49511cfcab1126c7bfb6886d15ff5c7c0
Romain Endelin <rendelin@zendesk.com>
2018-03-10 22:18:22 +0100

    Setup TapEventPlugin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
-// import injectTapEventPlugin from "react-tap-event-plugin"
+import injectTapEventPlugin from "react-tap-event-plugin"
 import "babel-polyfill"
 
 import Main from "./Main"
 
-// injectTapEventPlugin()
+injectTapEventPlugin()
 
 ReactDOM.render(
   <Main />,
