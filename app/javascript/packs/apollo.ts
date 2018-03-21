bdcd705869cf7217773376c8e600a6c31d0c2ee8
Romain Endelin <rendelin@zendesk.com>
2018-03-21 19:22:26 +0100

    Add JWT token to apollo calls

--- app/javascript/packs/apollo.ts
+++ app/javascript/packs/apollo.ts
@@ -1,8 +1,26 @@
 import { InMemoryCache } from "apollo-cache-inmemory"
 import { ApolloClient } from "apollo-client"
+import { ApolloLink } from "apollo-link"
 import { HttpLink } from "apollo-link-http"
+
+import { AUTH_TOKEN } from './constants'
+
+const httpLink = new HttpLink({ uri: "/api/graphql" })
+
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      Authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
 
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({ uri: "/api/graphql" })
+  link: httpLinkWithAuthToken
 })
